f18d5857f899f5b2261764cb8100cf4769771a8a
= <tamara.afvieira@gmail.com>
2023-05-27 16:43:51 -0300

    resultado escolar com condicao ternaria reduzida

--- Nova pasta/controle-fluxo/src/ResultadoEscolar.java
+++ Nova pasta/controle-fluxo/src/ResultadoEscolar.java
@@ -1,13 +1,8 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
-       
-        if(nota >= 7)
-        System.out.println("Aprovado");
-
-        else
-        System.out.println("Reprovado");
-
+        int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
 
     }
 }
