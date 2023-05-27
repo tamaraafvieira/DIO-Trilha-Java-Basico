d0dbc86d56053416e196b83cc50de937200ed575
= <tamara.afvieira@gmail.com>
2023-05-27 16:22:24 -0300

    saldo do caixa eletronico

--- Nova pasta/controle-fluxo/src/CaixaEletronico.java
+++ Nova pasta/controle-fluxo/src/CaixaEletronico.java
@@ -1,15 +1,17 @@
+import javax.swing.plaf.synth.SynthSplitPaneUI;
+
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 27.0;
 
-        if(valorSolicitado < saldo)
-        saldo = saldo - valorSolicitado;
-
-        System.out.println(saldo);
-
-
-
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo " + saldo);
+        }else{
+            System.out.println("Saldo insuficiente, saldo: " + saldo);
+        }
+       
 
     }
 }
